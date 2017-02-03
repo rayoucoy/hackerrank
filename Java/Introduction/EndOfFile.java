e99a26a79291b81d370e912c578238abd8cad237
Yudha Nur Rizki <rayoucoy@users.noreply.github.com>
2017-02-04 01:31:49 +0700

    Create EndOfFile.java

---
+++ Java/Introduction/EndOfFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int line = 1;
+        
+        for( ; sc.hasNext(); line++){
+            
+            System.out.println(String.valueOf(line) + " " + sc.nextLine());
+        }
+    }
+}
